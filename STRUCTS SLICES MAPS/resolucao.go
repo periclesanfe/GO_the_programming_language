424e273b60abeb69bef9cfa8ad34e98d92ca9b26
Péricles Andrade Feitoza <periclesanfe@gmail.com>
2023-05-02 00:13:19 -0300

    resolução fibonacci

---
+++ STRUCTS SLICES MAPS/resolucao.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// fibonacci é uma função que retorna uma função que retorna um int.
+func fibonacci() func() int {
+	a, b := 0, 1 // inicializa as variáveis para a série de Fibonacci
+	return func() int {
+		result := a
+		a, b = b, a+b // atualiza os valores para a próxima iteração
+		return result
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
