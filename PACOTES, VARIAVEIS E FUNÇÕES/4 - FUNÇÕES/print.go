510260b64ed27ec4f375c2665c66da3d7bea50c8
Péricles Andrade Feitoza <periclesanfe@gmail.com>
2023-05-03 09:54:55 -0300

    Create print.go

---
+++ PACOTES, VARIAVEIS E FUNÇÕES/4 - FUNÇÕES/print.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Imprime uma string
+	fmt.Println("Hello, world!")
+
+	// Imprime um número inteiro
+	fmt.Println(42)
+
+	// Imprime um número de ponto flutuante
+	fmt.Println(3.14)
+
+	// Imprime um valor booleano
+	fmt.Println(true)
+
+	// Imprime um valor de caractere
+	fmt.Println('A')
+
+	// Imprime uma string formatada com um valor inteiro
+	fmt.Printf("O valor de x é %d\n", 10)
+
+	// Imprime uma string formatada com um valor de ponto flutuante
+	fmt.Printf("O valor de pi é %f\n", 3.14159)
+
+	// Imprime uma string formatada com um valor booleano
+	fmt.Printf("O valor de flag é %t\n", true)
+
+	// Imprime uma string formatada com um valor de caractere
+	fmt.Printf("O valor de char é %c\n", 'A')
+}
